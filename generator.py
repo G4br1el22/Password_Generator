b7c11215dc0ae8080ae757210437728896adb23c
G4br1el22 <ga1755819@gmail.com>
2026-04-20 15:21:51 -0300

    REFACTOR: clean up code and fix minor issues

--- generator.py
+++ generator.py
@@ -4,48 +4,42 @@ import string
 
 
 # Randomizing function
-def create_password(len, char):
+def create_password(length, char):
     random_string = ""
-    for i in range(len):
+    for i in range(length):
         random_string += random.choice(char)
     return random_string
 
 
-lenght = int(input("Type your password lenght: "))
+# Password length
+
+length = int(input("Type your password lenght: "))
+
+# Setting and building characters
+
 characters = ""
 
 print("Choose character types that are going to be in your password:")
-letters = str(input("Letters y/n: "))
-digits = str(input("Digits y/n: "))
-symbols = str(input("Symbols y/n: "))
+letters = str(input("Letters y/n: ")).lower()
+digits = str(input("Digits y/n: ")).lower()
+symbols = str(input("Symbols y/n: ")).lower()
 
 # Letters
-
 if letters == "y":
     characters += string.ascii_letters
-elif letters == "n":
-    pass
-else:
-    print("Please select a valid option")
 
 # Digits
-
 if digits == "y":
     characters += string.digits
-elif digits == "n":
-    pass
-else:
-    print("Please select a valid option")
 
 # Symbols
-
 if symbols == "y":
     characters += string.punctuation
-elif symbols == "n":
-    pass
+
+# Validation
+
+if characters == "":
+    print("You must select at least one option!")
 else:
-    print("Please select a valid option")
-
-
-# Printing password
-print(create_password(lenght, characters))
+    # Printing password
+    print(create_password(length, characters))
